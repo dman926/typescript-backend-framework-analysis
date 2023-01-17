b2649ee466fcb23d107a4784fb4fb58eaf12b1eb
Dennis James Stelmach <dj@dstelmach.com>
2023-01-17 12:42:12 -0500

    Complete inital stress-tester and api applications.

--- packages/express-api/src/main.ts
+++ packages/express-api/src/main.ts
@@ -2,9 +2,22 @@ import express from 'express';
 const app = express();
 
 app.get('/', (_req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: 'Express API GET' });
 });
 
-app.listen(3000, () => {
-  // Server is running
+app.post('/', (_req, res) => {
+  res.send({ message: 'Express API POST' });
+});
+
+const server = app.listen(3000, () => {
+  console.log('Express started');
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.post('/shutdown', (_req, res) => {
+  res.send('ok');
+  server.close(() => {
+    console.log('Express shutting down');
+    process.exit(0);
+  });
 });
--- packages/fastify-api/src/main.ts
+++ packages/fastify-api/src/main.ts
@@ -4,12 +4,29 @@ const app = fastify();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.get('/', async (_req, _res) => {
-  return { message: 'Hello API' };
+  return { message: 'Fastify API GET' };
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.post('/', async (_req, _res) => {
+  return { message: 'Fastify API POST' };
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.post('/shutdown', (_req, res) => {
+  res.send('ok');
+  app.close(() => {
+    console.log('Fastify shutting down');
+    process.exit(0);
+  });
 });
 
 const start = async () => {
   try {
-    await app.listen({ port: 3000 });
+    app.server.on('listening', () => {
+      console.log('Fastify started');
+    });
+    await app.listen({ port: 3001 });
   } catch (err) {
     // Errors are logged here
     process.exit(1);
